89c017e9c893348293ec57258614c8818c00739e
Steph Locke <steph@itsalocke.com>
2017-05-14 18:17:36 +0100

    Move to .output.yml

--- buildpres.R
+++ buildpres.R
@@ -1,11 +1,4 @@
 library(revealjs)
 slides=list.files("pres","*.Rmd",full.names=TRUE)
 
-for (f in slides) render(f,output_dir = "docs",
-                output_format = revealjs::revealjs_presentation(
-                  theme = "lockedata",
-                  reveal_options = list(height="1080",
-                                        width="1920",
-                                        margin="0.05")
-                  )
-)
+for (f in slides) render(f,output_dir = "docs")
