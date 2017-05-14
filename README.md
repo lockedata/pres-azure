13877022bae083c8e3f29dcd46804e3fd9f23f1e
Steph Locke <steph@itsalocke.com>
2017-05-14 17:49:37 +0100

    Infrastructure

---
+++ README.md
@@ -0,0 +1,8 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+Presentation Stub
+=================
+
+This is the core infrastructure for a repo containing presentations made by Locke Data.
+
+Travis-CI will build the package (mainly for getting dependencies!) then will render each presentation and commit the docs to github.
--- buildpres.sh
+++ buildpres.sh
@@ -5,10 +5,9 @@ GITURL="https://$GITHUB_PAT@github.com/$TRAVIS_REPO_SLUG.git"
 
 git remote set-url origin $GITURL
 
-for f in $(find pres -name ".Rmd")
-do
-  Rscript -e 'rmarkdown::render("$f", output_dir="docs")'
-done 
+R CMD BATCH './buildpres.R'
+
+cp buildpres.Rout docs
 
 git add .
 git commit -am "[ci skip] Documents produced in clean environment via Travis $TRAVIS_BUILD_NUMBER"
