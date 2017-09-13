0c5f01c44216fabd7b5858424260e5a6551f6597
Steph <Steph@itsalocke.com>
2017-09-13 11:53:33 +0100

    de:bug:

--- odbcinstall.sh
+++ odbcinstall.sh
@@ -1,4 +1,4 @@
-wget -qO - https://packages.microsoft.com/keys/microsoft.asc
+wget -qO - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/16.04/prod xenial main" > /etc/apt/sources.list.d/mssql-release.list
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql
